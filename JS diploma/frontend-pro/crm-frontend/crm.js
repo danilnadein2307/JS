ee8ccf8f338150ec4c1ce33de5724e84525b30e5
unknown <danil.nadein@yandex.ru>
2024-07-27 23:59:35 +0300

    1

--- JS diploma/frontend-pro/crm-frontend/crm.js
+++ JS diploma/frontend-pro/crm-frontend/crm.js
@@ -14,9 +14,9 @@ document.addEventListener("DOMContentLoaded", async function() {
   const clientsHeadline = document.createElement("h1");
   clientsHeadline.innerHTML = "Клиенты";
   const clientsTable = document.createElement("table");
-  clientsTable.classList.add('clientsTable')
+  clientsTable.classList.add("clientsTable");
   const clientsTableHead = document.createElement("thead"); // голова таблицы
-  clientsTableHead.classList.add('clientsTable_head')
+  clientsTableHead.classList.add("clientsTable_head");
   const headRow = document.createElement("tr");
   headRow.classList.add("head_row");
   const clientIdCol = document.createElement("th");
@@ -32,7 +32,6 @@ document.addEventListener("DOMContentLoaded", async function() {
   clientChangeActionsCol.innerHTML = "Действия";
   const clientsTableBody = document.createElement("tbody"); // тело таблицы
 
-
   const addClientButton = document.createElement("button");
   addClientButton.classList.add("add_client_button");
   const addClientImg = document.createElement("img");
@@ -40,16 +39,15 @@ document.addEventListener("DOMContentLoaded", async function() {
   addClientImg.src = "img/addClient.png";
   addClientButton.textContent = "Добавить клиента";
 
-
   header.append(logo, headerSearchInput);
-  headRow.append(clientIdCol,
+  headRow.append(
+    clientIdCol,
     clientFullnameCol,
     clientTimeCreationCol,
     clientContactsCol,
-    clientChangeActionsCol)
-    clientsTableHead.append(
-      headRow
-    );
+    clientChangeActionsCol
+  );
+  clientsTableHead.append(headRow);
   clientsTable.append(clientsTableHead, clientsTableBody);
   addClientButton.append(addClientImg);
   container.append(clientsHeadline, clientsTable, addClientButton);
@@ -57,49 +55,44 @@ document.addEventListener("DOMContentLoaded", async function() {
 
   function addClient(clientObj) {
     const clientInformationRow = document.createElement("tr");
-    clientInformationRow.classList.add('client_row')
+    clientInformationRow.classList.add("client_row");
     const clientIdCell = document.createElement("td");
-    clientIdCell.classList.add('client_cell')
+    clientIdCell.classList.add("client_cell");
     const clientFullNameCell = document.createElement("td");
-    clientFullNameCell.classList.add('client_cell')
+    clientFullNameCell.classList.add("client_cell");
     const clientCreationTimeCell = document.createElement("td");
-    clientCreationTimeCell.classList.add('client_cell')
+    clientCreationTimeCell.classList.add("client_cell");
     const clientContactsCell = document.createElement("td");
-    clientContactsCell.classList.add('client_cell')
+    clientContactsCell.classList.add("client_cell");
     const clientActionsCell = document.createElement("td");
-    clientActionsCell.classList.add('client_cell')
-    const clientActionsButtonGroup = document.createElement('div')
-    const clientDeleteButton = document.createElement('button')
-    clientDeleteButton.textContent = 'Удалить'
-    clientDeleteButton.classList.add('clientAction_button')
-    const clientDeleteImg = document.createElement('img')
-    clientDeleteImg.classList.add('clientDelete_img')
-    clientDeleteImg.src = 'img/cancel.png'
-    const clientEditButton = document.createElement('button')
-    clientEditButton.textContent = 'Изменить'
-    clientEditButton.classList.add('clientAction_button')
-    const clientEditImg = document.createElement('img')
-    clientEditImg.src = 'img/edit.png'
-    clientEditImg.classList.add('clientEdit_img')
-    
+    clientActionsCell.classList.add("client_cell");
+    const clientActionsButtonGroup = document.createElement("div");
+    const clientDeleteButton = document.createElement("button");
+    clientDeleteButton.textContent = "Удалить";
+    clientDeleteButton.classList.add("clientAction_button");
+    const clientDeleteImg = document.createElement("img");
+    clientDeleteImg.classList.add("clientDelete_img");
+    clientDeleteImg.src = "img/cancel.png";
+    const clientEditButton = document.createElement("button");
+    clientEditButton.textContent = "Изменить";
+    clientEditButton.classList.add("clientAction_button");
+    const clientEditImg = document.createElement("img");
+    clientEditImg.src = "img/edit.png";
+    clientEditImg.classList.add("clientEdit_img");
 
-    clientIdCell.textContent = clientObj.id
-    clientFullNameCell.textContent = `${clientObj.surname} ${clientObj.name} ${clientObj.lastName}`.trim()
-    clientCreationTimeCell.textContent = clientObj.createdAt
-    clientDeleteButton.addEventListener('click' , function(){
-      clientDelete(clientObj.id)
-      clientInformationRow.remove()
-    })
+    clientIdCell.textContent = clientObj.id;
+    clientFullNameCell.textContent = `${clientObj.surname} ${clientObj.name} ${clientObj.lastName}`.trim();
+    clientCreationTimeCell.textContent = clientObj.createdAt;
+    clientDeleteButton.addEventListener("click", function() {
+      clientDelete(clientObj.id);
+      clientInformationRow.remove();
+    });
     // clientContactsCell.textContent = 'VK'
 
-
-
-
-
-    clientEditButton.append(clientEditImg)
-    clientDeleteButton.append(clientDeleteImg)
-    clientActionsButtonGroup.append(clientEditButton, clientDeleteButton)
-    clientActionsCell.append(clientActionsButtonGroup)
+    clientEditButton.append(clientEditImg);
+    clientDeleteButton.append(clientDeleteImg);
+    clientActionsButtonGroup.append(clientEditButton, clientDeleteButton);
+    clientActionsCell.append(clientActionsButtonGroup);
     clientInformationRow.append(
       clientIdCell,
       clientFullNameCell,
@@ -110,15 +103,12 @@ document.addEventListener("DOMContentLoaded", async function() {
     clientsTableBody.append(clientInformationRow);
   }
 
-  
-
-
   async function clientPost(obj) {
     try {
       const response = await fetch("http://localhost:3000/api/clients", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(obj)
+        body: JSON.stringify(obj),
       });
       const result = await response.json();
       return result;
@@ -131,23 +121,21 @@ document.addEventListener("DOMContentLoaded", async function() {
       const data = await response.json();
       return data;
     } catch {}
-
   }
 
   async function clientDelete(id) {
     await fetch(`http://localhost:3000/api/clients/${id}`, {
-      method: "DELETE"
+      method: "DELETE",
     });
   }
 
   function createClientsTable(arr) {
-    for(const newClient of arr) {
-      addClient(newClient)
+    for (const newClient of arr) {
+      addClient(newClient);
     }
   }
-  
-  createClientsTable(clientsArray)  
 
+  createClientsTable(clientsArray);
 
   function createClientCreationModal() {
     const clientCreationModal = document.createElement("div");
@@ -177,6 +165,8 @@ document.addEventListener("DOMContentLoaded", async function() {
     const lastNameInput = document.createElement("input");
     lastNameInput.placeholder = "Отчество";
     lastNameInput.classList.add("newClient_form_input");
+    const addContactGroup = document.createElement("div");
+    addContactGroup.classList.add("addContact_group");
     const addContactButton = document.createElement("button");
     addContactButton.innerHTML = "Добавить контакт";
     addContactButton.classList.add("addContact_button");
@@ -184,6 +174,8 @@ document.addEventListener("DOMContentLoaded", async function() {
     addContactImg.classList.add("addContact_img");
     addContactImg.src = "img/addContact.png";
     addContactButton.classList.add("addContact_button");
+    const newClientFormButtonGroup = document.createElement("div");
+    newClientFormButtonGroup.classList.add("newClientForm_button-group");
     const saveNewClientButton = document.createElement("input");
     saveNewClientButton.type = "submit";
     saveNewClientButton.classList.add("saveNewClient_button");
@@ -196,13 +188,17 @@ document.addEventListener("DOMContentLoaded", async function() {
 
     closeModalButton.append(closeImg);
     addContactButton.append(addContactImg);
+    addContactGroup.append(addContactButton);
+    newClientFormButtonGroup.append(
+      saveNewClientButton,
+      cancelNewClientInformationButton
+    );
     addNewClientForm.append(
       surnameInput,
       nameInput,
       lastNameInput,
-      addContactButton,
-      saveNewClientButton,
-      cancelNewClientInformationButton
+      addContactGroup,
+      newClientFormButtonGroup
     );
     clientCreationModalHead.append(newClientHeadline, closeModalButton);
     clientCreationModalContent.append(
@@ -224,30 +220,54 @@ document.addEventListener("DOMContentLoaded", async function() {
       }
     };
 
-    addNewClientForm.addEventListener('submit', function(event) {
-    event.preventDefault()
-    const client = {
-      name: nameInput.value,
-      surname: surnameInput.value,
-      lastName: lastNameInput.value,
-      // contacts: [{vk: '11'}, {facebook: '22'}, {mail: '22'}],
-    }
-    const createClient = async () => {
-      const newClient = await clientPost(client);
-      clientsArray.push(newClient);
-      addClient(newClient);
-      clientCreationModal.style.display = "none";
-    };
-    createClient();
-    console.log(clientsArray)
-
-    })
+    const dropdownOptions = ["Телефон", "Email", "Facebook", "VK", "Другое"];
     
+    function dropdownCreate() {
+      const contactInformationGroup = document.createElement("div");
+      contactInformationGroup.classList.add("contactInformation_group");
+      const dropdown = document.createElement("select");
+      dropdown.classList.add("dropdown");
+      const contactInfoInput = document.createElement("input");
+      contactInfoInput.classList.add("contactInfo_input");
+      contactInfoInput.placeholder = "Введите данные контакта";
+      for (let i = 0; i < dropdownOptions.length; ++i) {
+        const dropdownOption = document.createElement("option");
+        dropdownOption.classList.add('dropdown_option')
+        dropdownOption.textContent = dropdownOptions[i];
+        dropdown.append(dropdownOption);
+      }
+
+      contactInformationGroup.append(dropdown, contactInfoInput);
+      addContactGroup.append(contactInformationGroup);
+    }
+    let counter = 0;
+    addContactButton.addEventListener("click", function(event) {
+      event.preventDefault();
+      dropdownCreate();
+      counter += 1;
+      if (counter === 10) {
+        addContactButton.remove();
+      }
+    });
+    saveNewClientButton.addEventListener("click", function(event) {
+      event.preventDefault();
+      const client = {
+        name: nameInput.value,
+        surname: surnameInput.value,
+        lastName: lastNameInput.value,
+        contacts: [],
+      };
+      
+      console.log(client.contacts)
+      const createClient = async () => {
+        const newClient = await clientPost(client);
+        clientsArray.push(newClient);
+        addClient(newClient);
+        clientCreationModal.style.display = "none";
+      };
+      createClient();
+      console.log(clientsArray);
+    });
   }
   createClientCreationModal();
-
- 
-
-  
-
 });
